7437df124b4309aa5c46d8d2d00f72740c898f02
nayan <sontiya.nayan@gmail.com>
2023-07-16 19:44:02 +0530

    upload

--- src/layouts/MainLayout/index.jsx
+++ src/layouts/MainLayout/index.jsx
@@ -1,4 +1,14 @@
-const MainLayout = ({ children }) => {
-  return <>{children}</>;
+import { Container } from "react-bootstrap";
+import Header from "../../components/Structure/Header";
+import Footer from "../../components/Structure/Footer";
+
+const MainLayout = ({ children, padding }) => {
+  return (
+    <Container fluid className="p-0 position-relative">
+      <Header padding={padding} />
+      {children}
+      <Footer />
+    </Container>
+  );
 };
 export default MainLayout;
